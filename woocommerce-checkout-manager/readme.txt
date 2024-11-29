26a029be78d507a7ed706a8d4d1a8e55b6be730c
Justin Gardner <justin@rhynorater.com>
2024-11-29 06:02:43 -0500

    7.6.2

--- woocommerce-checkout-manager/readme.txt
+++ woocommerce-checkout-manager/readme.txt
@@ -4,12 +4,12 @@ Donate link: https://quadlayers.com/products/woocommerce-checkout-manager/
 Tags: woocommerce checkout, woocommerce checkout field, checkout field editor, checkout manager, checkout field customizer
 Requires at least: 4.7
 Requires PHP: 5.6
-Tested up to: 6.6
-Stable tag: 7.6.0
+Tested up to: 6.7
+Stable tag: 7.6.2
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 4.0
-WC tested up to: 9.3
+WC tested up to: 9.4
 
 Checkout Field Manager (Checkout Manager) for WooCommerce is the best plugin most advanced "Checkout Manager" to customize checkout fields on your WooCommerce checkout page.
 
@@ -152,6 +152,9 @@ Your Order data can be reviewed in each order within the default WooCommerce Ord
 10. Handle uploaded files in order admin dashboard.
 
 == Changelog ==
+
+=  7.6.2
+* WooCommerce compatibility
 
 =  7.6.0
 * WooCommerce compatibility
--- woocommerce-checkout-manager/vendor_packages/wp-notice-plugin-promote.php
+++ woocommerce-checkout-manager/vendor_packages/wp-notice-plugin-promote.php
@@ -4,7 +4,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	/**
 	 *  Promote constants
 	 */
-	define( 'WOOCCM_PROMOTE_LOGO_SRC', plugins_url( '/assets/backend/img/logo.jpg', WOOCCM_PLUGIN_FILE ) );
+	define( 'WOOCCM_PROMOTE_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-checkout-manager.jpg', WOOCCM_PLUGIN_FILE ) );
 	/**
 	 * Notice review
 	 */
@@ -15,7 +15,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	define( 'WOOCCM_PROMOTE_PREMIUM_SELL_SLUG', 'woocommerce-checkout-manager-pro' );
 	define( 'WOOCCM_PROMOTE_PREMIUM_SELL_NAME', 'WooCommerce Checkout Manager PRO' );
 	define( 'WOOCCM_PROMOTE_PREMIUM_SELL_URL', WOOCCM_PREMIUM_SELL_URL );
-	define( 'WOOCCM_PROMOTE_PREMIUM_INSTALL_URL', 'https://quadlayers.com/product/woocommerce-checkout-manager/?utm_source=wooccm_admin' );
+	define( 'WOOCCM_PROMOTE_PREMIUM_INSTALL_URL', 'https://quadlayers.com/products/woocommerce-checkout-manager/?utm_source=wooccm_admin' );
 	/**
 	 * Notice cross sell 1
 	 */
@@ -23,6 +23,8 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_1_NAME', 'Direct Checkout' );
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_1_DESCRIPTION', esc_html__( 'Direct Checkout for WooCommerce allows you to reduce the steps in the checkout process by skipping the shopping cart page. This can encourage buyers to shop more and quickly. You will increase your sales reducing cart abandonment.', 'woocommerce-checkout-manager' ) );
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_1_URL', 'https://quadlayers.com/products/woocommerce-direct-checkout/?utm_source=wooccm_admin' );
+	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_1_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-direct-checkout.jpg', WOOCCM_PLUGIN_FILE ) );
+
 	/**
 	 * Notice cross sell 2
 	 */
@@ -30,27 +32,22 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_2_NAME', 'Perfect WooCommerce Brands' );
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_2_DESCRIPTION', esc_html__( 'Perfect WooCommerce Brands the perfect tool to improve customer experience on your site. It allows you to highlight product brands and organize them in lists, dropdowns, thumbnails, and as a widget.', 'woocommerce-checkout-manager' ) );
 	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_2_URL', 'https://quadlayers.com/products/perfect-woocommerce-brands/?utm_source=wooccm_admin' );
+	define( 'WOOCCM_PROMOTE_CROSS_INSTALL_2_LOGO_SRC', plugins_url( '/assets/backend/img/perfect-woocommerce-brands.jpg', WOOCCM_PLUGIN_FILE ) );
 
 	new \QuadLayers\WP_Notice_Plugin_Promote\Load(
 		WOOCCM_PLUGIN_FILE,
 		array(
 			array(
 				'type'               => 'ranking',
-				'notice_delay'       => MONTH_IN_SECONDS,
+				'notice_delay'       => 0,
 				'notice_logo'        => WOOCCM_PROMOTE_LOGO_SRC,
-				'notice_title'       => sprintf(
-					esc_html__(
-						'Hello! Thank you for choosing the %s plugin!',
-						'woocommerce-checkout-manager'
-					),
-					WOOCCM_PLUGIN_NAME
+				'notice_description' => sprintf(
+								esc_html__( 'Hello! %2$s We\'ve spent countless hours developing this free plugin for you and would really appreciate it if you could drop us a quick rating. Your feedback is extremely valuable to us. %3$s It helps us to get better. Thanks for using %1$s.', 'woocommerce-checkout-manager' ),
+								'<b>'.WOOCCM_PLUGIN_NAME.'</b>',
+								'<span style="font-size: 16px;">🙂</span>',
+								'<br>'
 				),
-				'notice_description' => esc_html__( 'Could you please give it a 5-star rating on WordPress? Your feedback boosts our motivation, helps us promote, and continues to improve this product. Your support matters!', 'woocommerce-checkout-manager' ),
 				'notice_link'        => WOOCCM_PROMOTE_REVIEW_URL,
-				'notice_link_label'  => esc_html__(
-					'Yes, of course!',
-					'woocommerce-checkout-manager'
-				),
 				'notice_more_link'   => WOOCCM_SUPPORT_URL,
 				'notice_more_label'  => esc_html__(
 					'Report a bug',
@@ -64,7 +61,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					'Purchase Now',
 					'woocommerce-checkout-manager'
 				),
-				'notice_delay'       => MONTH_IN_SECONDS,
+				'notice_delay'       => WEEK_IN_SECONDS,
 				'notice_logo'        => WOOCCM_PROMOTE_LOGO_SRC,
 				'notice_title'       => esc_html__(
 					'Hello! We have a special gift!',
@@ -80,15 +77,11 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					WOOCCM_PROMOTE_PREMIUM_SELL_NAME
 				),
 				'notice_more_link'   => WOOCCM_PROMOTE_PREMIUM_SELL_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'woocommerce-checkout-manager'
-				),
 			),
 			array(
 				'plugin_slug'        => WOOCCM_PROMOTE_CROSS_INSTALL_1_SLUG,
-				'notice_delay'       => MONTH_IN_SECONDS * 4,
-				'notice_logo'        => WOOCCM_PROMOTE_LOGO_SRC,
+				'notice_delay'       => MONTH_IN_SECONDS * 3,
+				'notice_logo'        => WOOCCM_PROMOTE_CROSS_INSTALL_1_LOGO_SRC,
 				'notice_title'       => sprintf(
 					esc_html__(
 						'Hello! We want to invite you to try our %s plugin!',
@@ -97,16 +90,12 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					WOOCCM_PROMOTE_CROSS_INSTALL_1_NAME
 				),
 				'notice_description' => WOOCCM_PROMOTE_CROSS_INSTALL_1_DESCRIPTION,
-				'notice_more_link'   => WOOCCM_PROMOTE_CROSS_INSTALL_1_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'woocommerce-checkout-manager'
-				),
+				'notice_more_link'   => WOOCCM_PROMOTE_CROSS_INSTALL_1_URL
 			),
 			array(
 				'plugin_slug'        => WOOCCM_PROMOTE_CROSS_INSTALL_2_SLUG,
 				'notice_delay'       => MONTH_IN_SECONDS * 6,
-				'notice_logo'        => WOOCCM_PROMOTE_LOGO_SRC,
+				'notice_logo'        => WOOCCM_PROMOTE_CROSS_INSTALL_2_LOGO_SRC,
 				'notice_title'       => sprintf(
 					esc_html__(
 						'Hello! We want to invite you to try our %s plugin!',
@@ -115,11 +104,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					WOOCCM_PROMOTE_CROSS_INSTALL_2_NAME
 				),
 				'notice_description' => WOOCCM_PROMOTE_CROSS_INSTALL_2_DESCRIPTION,
-				'notice_more_link'   => WOOCCM_PROMOTE_CROSS_INSTALL_2_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'woocommerce-checkout-manager'
-				),
+				'notice_more_link'   => WOOCCM_PROMOTE_CROSS_INSTALL_2_URL
 			),
 		)
 	);
